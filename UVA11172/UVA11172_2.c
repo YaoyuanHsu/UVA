9fa157ca50161555ab2a9377fd0de9f14b64dec8
YaoyuanHsu <33758678+YaoyuanHsu@users.noreply.github.com>
2019-07-16 15:03:35 +0800

    Create UVA11172_2.c

---
+++ UVA11172/UVA11172_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a , b , c ;
+	for( scanf("%d" , &a); a--!=0 && scanf("%d %d" , &b , &c) ;){
+		if(b==c)
+            printf("=\n");
+		else if(b>c)
+            printf(">\n");
+        else printf("<\n");
+    }
+}
